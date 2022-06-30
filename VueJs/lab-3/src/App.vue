a546a46c0db9a29a8440a746bb44cc2fa1dfb636
HassanTawfic <hatawfic@gmail.com>
2022-06-30 16:16:06 +0200

    lab3

---
+++ VueJs/lab-3/src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="container">
+    <div class="d-flex align-items-center justify-content-center"></div>
+  <div>
+    <p class="display-2">Lab-3</p>
+  </div>
+  <div>
+    <button class="btn btn-danger mx-2" @click.prevent="AdminPanel">Show Admin</button>
+    <button class="btn btn-primary mx-2" @click.prevent="FormPanel">Show Form</button>
+    <button class="btn btn-success mx-2" @click.prevent="StudentPanel">Show Student</button>
+  </div>
+  <div class="mt-5">
+    <div>
+      <FormApp v-if="showForm===true" @send="receiveData"/>
+    </div>
+
+  <StudentApp v-if="showStudent===true"/>
+  <AdminApp v-if="showAdmin===true"/>
+  </div>
+  </div>
+</template>
+
+<script>
+import FormApp from './components/FormApp.vue'
+import StudentApp from './components/StudentApp.vue'
+import AdminApp from './components/AdminApp.vue'
+import {ref} from "vue";
+
+export default {
+  name: 'App',
+  components: {
+    FormApp,
+    AdminApp,
+    StudentApp
+  },
+  setup() {
+    const Students = ref([])
+    const Admins = ref([])
+    const showForm = ref(true)
+    const showStudent = ref(false)
+    const showAdmin = ref(false)
+    const student_id = ref(0)
+    const admin_id = ref(0)
+    const FormPanel = () =>{
+      showForm.value = true
+      showStudent.value = false
+      showAdmin.value = false
+    }
+    const AdminPanel = () =>{
+      showForm.value = false
+      showStudent.value = false
+      showAdmin.value = true
+    }
+    const StudentPanel = () =>{
+      showForm.value = false
+      showStudent.value = true
+      showAdmin.value = false
+    }
+    const receiveData = (data) =>{
+      console.log(data)
+    }
+    return {
+      Students,
+      Admins,
+      showForm,
+      showStudent,
+      showAdmin,
+      student_id,
+      admin_id,
+      FormPanel,
+      AdminPanel,
+      StudentPanel,
+      receiveData
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VueJs/lab-3/src/components/FormApp.vue
@@ -0,0 +1,60 @@
+<template>
+    <form @click.prevent="sendFromValues">
+      <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input type="email" class="form-control" name="email" id="email" v-model="formValues.email">
+      </div>
+      <div class="mb-3">
+        <label for="age" class="form-label">Age</label>
+        <input type="number" class="form-control" name="age" id="age" v-model="formValues.age">
+      </div>
+      <div class="mb-3">
+        <label for="address" class="form-label">City</label>
+        <input type="text" class="form-control" name="address" id="address" v-model="formValues.city">
+      </div>
+      <div class="d-flex flex-column align-items-center">
+        <div class="form-check">
+          <input class="form-check-input" value="Admin" type="radio" name="flexRadioDefault" id="Admin" v-model="formValues.type">
+          <label class="form-check-label" for="Admin">
+            Admin
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" value="Student " type="radio" name="flexRadioDefault" id="Student" v-model="formValues.type">
+          <label class="form-check-label" for="Student">
+            Student
+          </label>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary mt-2">Submit</button>
+    </form>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+  name: "FormApp",
+  emits:['send'],
+  setup(props,context){
+    const formValues= ref({
+      id:0,
+      email:'',
+      age:'',
+      city:'',
+      type:''
+    })
+    const sendFromValues = () =>{
+      context.emit('send',formValues.value)
+    }
+    return{
+      formValues,
+      sendFromValues
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
